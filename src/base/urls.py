03df6ff0957008fee59bcc8c226781dc8ce0e1e3
karuna <karuna@involve.software>
2024-04-02 12:05:17 +0300

    EM-29: update base urlpatterns and add swagger endpoint

--- src/base/urls.py
+++ src/base/urls.py
@@ -1,13 +1,26 @@
 from django.contrib import admin
+from django.http import HttpResponse, HttpResponseNotAllowed
 from django.urls import include, path
+from django.views.static import serve as serve_static
+from rest_framework.request import Request
 
 from base import settings
 
 
+def get_swagger(request: Request) -> HttpResponse:
+    if request.method != "GET":
+        return HttpResponseNotAllowed(["GET"])
+
+    return serve_static(request, "swagger.json", document_root=settings.BASE_DIR)
+
+
 urlpatterns = [
-    path("", include("events.urls")),
+    # default urls
     path("admin/", admin.site.urls),
-    path("users/", include("users.urls")),
+    # api urls
+    path("api/", include("events.urls")),
+    path("api/swagger/", get_swagger, name="swagger"),
+    path("api/users/", include("users.urls")),
 ]
 
 
